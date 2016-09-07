0f9f90203e46afac768b796fef6ffc1b44b34e20
patelredg <patel.redg@gmail.com>
2016-09-07 22:19:41 +1000

    Set Programme Structure

--- DataMAnagement/src/datamanagement/Main.java
+++ DataMAnagement/src/datamanagement/Main.java
@@ -1,4 +1,8 @@
 package datamanagement;
 
 public class Main {
-    public static void main(String[] p) {new Cgctl().execute();}    }
+	
+    public static void main(String[] p) {new Cgctl().execute();}  
+    
+    }
+
